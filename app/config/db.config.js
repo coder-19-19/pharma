aadaa661f2ba7229a043c90e90a0c0e698c8ee21
coder-19-19 <76615028+coder-19-19@users.noreply.github.com>
2021-03-17 16:48:38 +0400

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "salamsalam1",
-    DB: "adnsu"
+    HOST: "sql301.epizy.com",
+    USER: "epiz_27574568",
+    PASSWORD: "7gr7GwT5zyj",
+    DB: "epiz_27574568_group"
   };
   
